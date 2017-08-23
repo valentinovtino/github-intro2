f6dbc7aadb6deb3f3530799705b87acf1c9da193
Valentino Valentin <valentinovalentine95@gmail.com>
2017-08-23 10:48:43 -0600

    MOVE UP

---
+++ index.js
@@ -0,0 +1,6 @@
+var textToChange = document.querySelector('.text-to-change')
+var buttonForText = document.querySelector('#text-button');
+
+buttonForText.addEventListener('click', function(){
+  textToChange.innerText = "You've Changed!";
+})
